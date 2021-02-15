0f5228dd323f281bdb1e02f77ebb9e3abfdaa490
Kevin Hallenbeck <khallenbeck@dataspeedinc.com>
2021-02-15 12:57:39 -0500

    Add Ford Edge platform

--- dbw_mkz_can/include/dbw_mkz_can/PlatformVersion.h
+++ dbw_mkz_can/include/dbw_mkz_can/PlatformVersion.h
@@ -48,6 +48,7 @@ typedef enum {
   P_FORD_C1     = 0x02, // Ford Transit Connect
   P_FORD_T6     = 0x03, // Ford Ranger
   P_FORD_U6     = 0x04, // Lincoln Aviator
+  P_FORD_CD5    = 0x05, // Ford Edge, Lincoln Nautilus
   P_FCA_RU      = 0x10, // Chrysler Pacifica
   P_FCA_WK2     = 0x11, // Jeep Grand Cherokee
   P_POLARIS_GEM = 0x80, // Polaris GEM
@@ -72,6 +73,7 @@ static const char* platformToString(Platform x) {
     case P_FORD_C1:     return "FORD_C1";
     case P_FORD_T6:     return "FORD_T6";
     case P_FORD_U6:     return "FORD_U6";
+    case P_FORD_CD5:    return "FORD_CD5";
     case P_FCA_RU:      return "FCA_RU";
     case P_FCA_WK2:     return "FCA_WK2";
     case P_POLARIS_GEM: return "POLARIS_GEM";
